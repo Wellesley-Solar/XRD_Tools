1f9384a7fb4f60524ad1dffea669543d9bf6c913
belisleb <60211792+belisleb@users.noreply.github.com>
2020-04-28 17:24:40 -0400

    Update oceanoptics.m
    now deletes empty files at begin of data acquisition

--- Matlab Programs/oceanoptics.m
+++ Matlab Programs/oceanoptics.m
@@ -31,14 +31,26 @@ for j=1:length(Tfiles)
     temp = readtable(CurrentFile, opts);
     data(:,1) = temp(:,1);
     data(:,j+1) = temp(:,2);
+    %To help me interpret when I've turned the light on (a.k.a. what is my
+    %first frame)
+    intensity(j) = sum(table2array(temp(:,2)));
+    frame(j) = j+1;
 
 %% Clear temporary variables
 clear opts
 end 
 
-SavedName =convertCharsToStrings(strcat(pathname,'oceanopticsdata.txt'));
-writetable(data, SavedName);
+%cut dead frames at beginning and end
+plot(frame,intensity);
+slope = gradient(intensity(:)) ./ gradient(frame(:));
+[a,b] = max(slope);
+data_sub = [data(:,1) data(:,b:end-5)];
+
+%export the data to the desired folder
+pathname = uigetdir;
+SavedName =convertCharsToStrings(strcat(pathname, Tfiles(j).name));
+writetable(data_sub, SavedName);
 end
 
-% need to get data to save
+
 % work on using outputted data in new function
