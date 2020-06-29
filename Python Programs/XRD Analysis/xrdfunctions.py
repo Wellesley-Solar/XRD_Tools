645b2e9d3f53c547172f73bdc5107670d7ea2756
Becky B <rbelisle@wellesley.edu>
2020-06-29 16:05:45 -0400

    Added comments to explain functions

--- Python Programs/XRD Analysis/xrdfunctions.py
+++ Python Programs/XRD Analysis/xrdfunctions.py
@@ -8,25 +8,38 @@ import pandas as pd
 # %%
 #Defining all functions that will be used in program
 def csv_to_np(filename):
+    #filemane is a csv of xrd data with the first column being two-theta 
+    # and subsequent columns being xray intensities
     data = pd.read_csv(filename)
     return(np.array(data))
 
 def two_to_q(two_theta, wave):
+    #two_theta is a 1D array of two_theta angles
+    #wave is the X-ray energy in angstroms
     rad_theta = two_theta/2*np.pi/180
     q = 4*np.pi*np.sin(rad_theta)/wave
     return q
 
 def find_nearest(array, value):
+    #array is a 1D vector of two_theta or q values
+    #value is the specific angle or q for which you want the index
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
 
 def trim_data(x, data, limit1, limit2):
+    #x is a 1D array of two theta or q values
+    #data is an array of x-ray intensities
+    #limit1 and limit2 are what you'd like to trime your data to 
     set1 = find_nearest(x,limit1)
     set2 = find_nearest(x,limit2)
     return x[set1:set2], data[set1:set2,:]
 
 def back_substract(x, data, length):
+    #x is a 1D array of two theta or q values
+    #data is an array of x-ray intensities
+    #length is the numper of values on the edges of the data you want to 
+    #use to create a linear background 
     x_linear = np.hstack((x[0:length], x[-length:-1])) #I'm taking the starting and ending values
     data_linear = np.hstack((data[0:length], data[-length:-1])) #We'll use these to fit a straight line
     slope, intercept = np.polyfit(x_linear, data_linear, 1) #Do linear fit
@@ -34,16 +47,20 @@ def back_substract(x, data, length):
     return data-back
 
 def gaussian(x, a, b, c): 
+    #generic gaussian curve, for XRD analysis:
+    #x is a 1D array of two theta or q values
+    #a is the max intensity of the peak and representative of crystalling
+    #b is the peak position and    
     return a*np.exp(-(x - b)**2/(2*c**2))
 
 def two_gaussians(x, a1, b1, c1, a2, b2, c2):
         return (gaussian(x, a1, b1, c1) +
             gaussian(x, a2, b2, c2))
 
-def multi_gaussian(x, trips):
+def multi_gaussian(x, guesses):
     # x is 1D array of 2-theta or q values for our fitting
     # trips is an array of fits i.e. [[200, 1, .01], [150, 1.05. .02]]
-    peaks = [gaussian(x, fit[0], fit[1], fit[2]) for fit in trips]
+    peaks = [gaussian(x, fit[0], fit[1], fit[2]) for fit in guesses]
     return np.sum(peaks, axis=0)
 
 
