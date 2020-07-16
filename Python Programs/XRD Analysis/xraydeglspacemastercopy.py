1a399ca5724916714d42a57cba97e82f3ac56b2a
gabehalford <61603844+gabehalford@users.noreply.github.com>
2020-07-16 12:04:19 -0400

    Update xraydeglspacemastercopy.py

--- Python Programs/XRD Analysis/xraydeglspacemastercopy.py
+++ Python Programs/XRD Analysis/xraydeglspacemastercopy.py
@@ -11,52 +11,27 @@ from scipy.optimize import curve_fit #for some reason I've found I have to call 
 from xrdfunctions import * #imports all functions in file named xrdfunctions
 
 #%%
-#Read csv file:
-def readcsv(filename):
-    data = pd.read_csv(filename)
-    return(np.array(data))
-
-#%%
-#Limiting to a Peak:
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin() #finds the index of the value closest to the target
-    return idx
-
-#%%
-#Single gaussian fit to plot
-def gaussian(x, height, center, width):
-    return height*np.exp(-(x - center)**2/(2*width**2)) 
-
-#%%
-#Turn two theta in q
-def two_to_q(two_theta, wave):
-    #two_theta is a 1D array of two_theta angles
-    #wave is the X-ray energy in angstroms
-    rad_theta = two_theta/2*np.pi/180
-    q = 4*np.pi*np.sin(rad_theta)/wave
-    return q
+#import csv file
+csv_to_np(put_filename_here)
+perov=np.array(data)
 
 #%%
 #Plotting initial frame of data
-#perov = readcsv('put_filename_here')
-perov = readcsv('/Users/rbelisle/Documents/GitHub/training/D1_MAPbIBr2_Xraydeg.csv') 
 plt.figure(figsize=(8,6)) #make plot larger
 plt.plot(perov[:,0],perov[:,1],'r-', label='$MAPbIBr_2$ initial') #plot two-theta versus XRD intensity
 plt.xlabel('2-theta [$^o$]',size=12) 
 plt.ylabel('Intensity [a.u.]',size=12)
 plt.title('initial')
 plt.legend(loc="upper right")
+plt.show()
 
 #%%
 #Define wavelength and convert two-theta to Angstroms
 #Note that our initial 2-theta values are in degrees, not radians
-#‘Two theta to q’
 q = two_to_q(perov[:,0],0.9763)
-Q= q.tolist() #QUESTION What is the motivation for making this a list?
-y=perov[:,1] #FEEDBACK chose more meaningful variable names. i.e. perov_plot
+perov_intensity=perov[:,1] 
 plt.figure(figsize=(8,6)) 
-plt.plot(Q,y, marker='.',color='r')
+plt.plot(Q,perov_intensity, marker='.',color='r')
 plt.title('Initial:')
 plt.xlabel('Q')
 plt.ylabel('Intensity')
@@ -64,21 +39,19 @@ plt.show()
 
 #%%
 #choose a peak and find its limits 
-#FEEDBACK check our new function called trim_data in XRD functions to do this - means you don't have to use a list anymore
-q_1 = .98
-q_2 = 1.15
-limit1 = Q.index(find_nearest(q, q_1))
-limit2 = Q.index(find_nearest(q, q_2))
-q_sub = q[limit1:limit2]
-perov_sub = perov[limit1:limit2,1:-1]# range will depend on the number of frames you have 
+trim_data(q, perov_intensity, limit1, limit2)
+q_1 = limit1
+q_2 = limit2
+q_sub = x[set1:set2]
+perov_sub = data[set1:set2,:]
 plt.plot(q_sub,perov_sub[:,-1])
+plt.show()
 
 #%%
 #remove background
 size = perov_sub.shape
 print (size)
 q_bins = size[0]
-#print (q_bins)
 num_frames = size[1]
 slope = np.zeros((num_frames,1))
 intercept = np.zeros((num_frames,1))
@@ -94,50 +67,47 @@ for i in range(num_frames):
     back[:,i] = [slope[i]*element+intercept[i] for element in q_sub]
     int_correct[:,i] = perov_sub[:,i]-np.array(back[:,i])
 plt.plot(np.array(q_sub),int_correct)
+plt.show()
 
 #%%
-#FEEDBACK for example imports I would have them be commmented next to inputs that will run...
-#makes it difficult to see how code executes otherwise 
-p0 = [200, 1, .01] #p0 = [height, center, width] 
+p0 = [200, q, .01] #p0 = [height, center, width] guesses
 intensity_1 = np.zeros((num_frames)) #create correct size arrays for running in the loop
-lattice_1= np.zeros((num_frames))
+lattice_1= np.zeros((num_frames)) 
 for j in range(num_frames):
-    popt, pcov = curve_fit(gaussian, np.array(q_sub), int_correct[:,j], p0)
-    intensity_1[j] = popt[0]
-    lattice_1[j] = 2*math.pi/popt[1] #need to fit for correct index
+    popt, pcov = curve_fit(gaussian, np.array(q_sub), int_correct[:,j], p0) #fit with gaussian function after giving guesses
+    intensity_1[j] = popt[0] #peak height
+    lattice_1[j] = 2*math.pi/popt[1] #center/lattice spacing #need to fit for correct index
     p0 = popt 
 
 #%%
-#plot
-#FEEDBACK this piece doens't seem super flexible, maybe you want to make it a function that takes a delta t...
-#and exports the correct data spacing
-time = np.zeros(num_frames) #create empty array for time of correct length
-for x in range(num_frames):
-    time[x] = x*20 + 20 #xray dose is 20s per frame
+#convert from frames to time
+time=frames_to_time(num_frames,speed,start_lag)
+#find speed and start_lag in tif text file
+
 
 #%%
-plt.figure(figsize=(8,6))
-plt.plot(time, lattice_1, marker='o') 
+plt.figure(figsize=(12,10))
+plt.plot(time, lattice_1, marker='bo') 
 plt.xlabel('time(s)')
 plt.ylabel('Lattice Spacing(angstrom)')
+plt.title('Lattice Spacing vs. Time')
+plt.xlim()
+plt.ylim(0,101)
+plt.legend(frameon=True, fancybox=True,framealpha=1, shadow=False, borderpad=1, 
+           title="figure_info", loc='lower right', fontsize='14')
 plt.show()
-#FEEDBACK I think these is an opportunity here to improve the formatting of your figure...
-#might want to look at some options for scaling etc. 
-#FEEDBACK Should we also be plotting intensity? 
-#FEEDBACK Program currently plots lattice spacing vs time on top of plot of XRD patterns... 
-#Update code so that it gives you a correct figure
-# %%
 
 
 #%%
-def br_frac (x) :
-    brfrac=(x - 6.256)/-0.347
-    if brfrac<0:
-        br=0
-    if brfrac>1:
-        br=100
-    else:
-        brpercent=100*br1
-    return brpercent
-#%%
+plt.figure(figsize=(12,10))
+plt.plot(time, intensity_1, marker='mo') 
+plt.xlabel('time(s)')
+plt.ylabel('Relative Intensity')
+plt.title('Intensity vs. Time')
+plt.xlim()
+plt.ylim()
+plt.legend(frameon=True, fancybox=True,framealpha=1, shadow=False, borderpad=1, 
+           title="figure_info", loc='lower right', fontsize='14')
+plt.show()
+
     
