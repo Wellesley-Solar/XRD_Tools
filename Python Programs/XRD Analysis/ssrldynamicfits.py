a7ef7ff940e6eab5dafb16aa3fec04fa98f10c4a
Becky B <rbelisle@wellesley.edu>
2020-07-22 19:00:34 -0400

    Fixed typo

--- Python Programs/XRD Analysis/ssrldynamicfits.py
+++ Python Programs/XRD Analysis/ssrldynamicfits.py
@@ -9,29 +9,28 @@ import pandas as pd
 
 #%% Import Data
 from xrdfunctions import *
-perov_import = csv_to_np('/Volumes/GoogleDrive/Shared drives/Wellesley Solar/Current Projects/ Hoke Effect/x0p33_data/Xray Degredation/B1_xraydeg.csv')
+perov_import = csv_to_np('/Users/rbelisle/Desktop/SSRL_data/B1_xraydeg.csv')
 
 #%% Covert to Q
 q = two_to_q(perov_import[:,0],0.9763)
-perov = perov_import[:,1:-1] #seperate XRD intensities from rest of data 
+perov = perov_import[:,1:] #seperate XRD intensities from rest of data 
 
 
 #%% #Trim and Remove Background
-miller = [2, 0, 0] #peak you are trying to capture
-q_1 = 1.9
-q_2 = 2.15
+miller = [1, 0, 0] #peak you are trying to capture
+q_1 = 0.84
+q_2 = .96
 q_sub, perov_sub = trim_data(q,perov,q_1,q_2)
 
 #%% #Loop through Data
-size = perov.shape
-files = size[1]
+files = num_files(perov)
 time = np.zeros(files)
 lattice = np.zeros(files)
 intensity = np.zeros(files)
-#files = num_files(perov)
-p0 = [20,2.01, 0.01] #best guess for number of peaks an initial values
+files = num_files(perov)
+p0 = [10,0.9, 0.01] #best guess for number of peaks an initial values
 for frame in range(files): 
-    perov_to_fit = back_substract(q_sub,perov_sub[:,frame],10)
+    perov_to_fit = back_subtract(q_sub,perov_sub[:,frame],10)
     popt, pcov = curve_fit(gaussian, q_sub, perov_to_fit, p0)
     intensity[frame] = popt[0]
     lattice[frame] = q_to_a(popt[1],miller)
@@ -47,4 +46,5 @@ plt.xlabel('Time [s]',size=14) #Define x-axis label
 plt.ylabel('Lattice Spacing [$\AA$]',size=14)#Define y-axis label
 plt.axis([0, 600, 6.14, 6.145])
 plt.savefig('MAPbI2Br_Xray_Lattice.png')
+
 # %%
--- Python Programs/XRD Analysis/ssrlstaticfits.py
+++ Python Programs/XRD Analysis/ssrlstaticfits.py
@@ -8,36 +8,37 @@ from scipy.optimize import curve_fit
 import pandas as pd
 
 #%% Import Data
-from xrdfunctions import *
-perov_import = csv_to_np('/Users/rbelisle/Desktop/SSRL_data/startend_full.csv')
+from xrdfunctions import csv_to_np, two_to_q, trim_data, num_files, back_subtract, pvoigt, gaussian, three_gaussians, q_to_a
+perov_import = csv_to_np('/Users/rbelisle/Desktop/SSRL_data/startend_wedge.csv')
 #%% Covert to Q
 q = two_to_q(perov_import[:,0],0.9763)
 perov = perov_import[:,1:] #seperate XRD intensities from rest of data 
 #%% #Trim and Remove Background
-miller = [2, 0, 0] #peak you are trying to capture
-q_1 = 1.92
-q_2 = 2.16
+miller = [1, 0, 0] #peak you are trying to capture
+q_1 = .96
+q_2 = 1.11
 q_sub, perov_sub = trim_data(q,perov,q_1,q_2)
 perov_fit = perov_sub
 files = num_files(perov_sub)
 for file in range(files): 
-    perov_fit[:,file] = back_substract(q_sub,perov_sub[:,file],10) #remove background from that file
+    perov_fit[:,file] = back_subtract(q_sub,perov_sub[:,file],10) #remove background from that file
 #%% Do Curve Fitting For One Peak
-image = 0 #index of file you want to look at
-p0 = [100, 2.04, .001] #best guess for initial values in format [a1, b1, c1, a2, c2, a3, b3, c3]
-upper_limit = [500, 2.2, 5]
-lower_limit = [0, 1.8, 0]
-popt,pcov = curve_fit(gaussian, q_sub, perov_fit[:,image], p0, bounds=(lower_limit, upper_limit), maxfev=6000)
+image = 10 #index of file you want to look at
+p0 = [0.2, 300, 1.02, .01] #best guess for initial values in format [a1, b1, c1, a2, c2, a3, b3, c3]
+upper_limit = [1, 3000, q_2, 5]
+lower_limit = [0, 0, q_1, 0]
+popt,pcov = curve_fit(pvoigt, q_sub, perov_fit[:,image], p0, bounds=(lower_limit, upper_limit), maxfev=6000)
 plt.plot(q_sub,perov_fit[:,image],'b-', label='Data') #plot subfield of data
-plt.plot(q_sub,gaussian(q_sub, *popt),'c--', label='Model') #plot best fit
+plt.plot(q_sub,pvoigt(q_sub, *popt),'c--', label='Model') #plot best fit
+init_q = popt[2]
 #%% Do Curve Fitting for Three Peaks
-image = 0 #index of file you want to look at
-init_q = 2.08389035 #q value from initial structure
-p0 = [5, 2.0, .01, 10, init_q, .001, 10, 2.10, .01] #best guess for initial values in format [a1, b1, c1, a2, c2, a3, b3, c3]
-upper_limit = [20, init_q, .3, 20, init_q+.0001, .3, 20, 2.15, .1] #upper limits fot the range where one peak is iodine rich and the other is bromine
-lower_limit = [0, 2.00, 0, 0, init_q-0.0001, 0, 0, init_q, 0]
+image = 11 #index of file you want to look at
+#init_q = 2.08389035 #q value from initial structure
+p0 = [5, 1.0, .01, 10, init_q, .001, 10, 1.05, .01] #best guess for initial values in format [a1, b1, c1, a2, c2, a3, b3, c3]
+upper_limit = [20, init_q, .3, 20, init_q+.0001, .3, 20, 1.08, .1] #upper limits fot the range where one peak is iodine rich and the other is bromine
+lower_limit = [0, 0.98, 0, 0, init_q-0.0001, 0, 0, init_q, 0]
 popt,pcov = curve_fit(three_gaussians, q_sub, perov_fit[:,image], p0, bounds=(lower_limit, upper_limit), maxfev=8000)
-# %% Plot everything
+# Plot everything
 plt.plot(q_sub,perov_fit[:,image],'b-', label='Data') #plot subfield of data
 plt.plot(q_sub,three_gaussians(q_sub, *popt),'c--', label='Model') #plot best fit
 plt.plot(q_sub, gaussian(q_sub, popt[0],popt[1],popt[2]), 'b-.', label='Peak 1')
@@ -48,3 +49,6 @@ plt.ylabel('Intensity [a.u.]',size=14)#Define y-axis label
 plt.legend(loc="upper right")#Put legend in upper left hand corner
 print('Intensity:', popt[0], popt[3], popt[6])
 print('Lattice Spacing:', q_to_a(popt[1],miller),q_to_a(popt[4],miller), q_to_a(popt[7],miller))
+
+
+# %%
